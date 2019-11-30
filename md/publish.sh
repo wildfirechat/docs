5c0d9bb18fb2a131159ae4f26d2ce547be99897b
heavyrain2012 <heavyrain.lee@gmail.com>
2019-11-30 09:08:05 +0800

    update

--- md/publish.sh
+++ md/publish.sh
@@ -1,6 +1,6 @@
 gitbook build
 mv _book wildfirechat_docs
 tar -czvf docs.tar.gz wildfirechat_docs
-scp docs.tar.gz dali2:~/
-ssh dali2 'rm -rf wildfirechat_docs; tar -xzvf docs.tar.gz'
+scp docs.tar.gz dali3:~/
+ssh dali3 'rm -rf wildfirechat_docs; tar -xzvf docs.tar.gz'
 rm -rf wildfirechat_docs docs.tar.gz
