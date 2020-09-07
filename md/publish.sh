5d3a855d048b05e002643c006476b9380b01bcbb
heavyrain2012 <heavyrain.lee@gmail.com>
2020-09-07 21:03:33 +0800

    add trickle ice

--- md/publish.sh
+++ md/publish.sh
@@ -1,5 +1,6 @@
 gitbook build
 mv _book wildfirechat_docs
+cp -af ../trickle-ice wildfirechat_docs/webrtc/
 tar -czvf docs.tar.gz wildfirechat_docs
 scp docs.tar.gz dali3:~/
 ssh dali3 'rm -rf wildfirechat_docs; tar -xzvf docs.tar.gz'
