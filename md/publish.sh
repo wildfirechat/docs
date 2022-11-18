e2510f5344afa0696e2b994cdedb7422a74b0184
imndx <imndxx@gmail.com>
2022-11-18 09:57:04 +0000

    update

--- md/publish.sh
+++ md/publish.sh
@@ -2,6 +2,7 @@ gitbook build
 mv _book docs
 cp -af ../trickle-ice docs/webrtc/
 cp -af ../abilitytest docs/webrtc/
+cp -af ../wstool docs/web/
 tar -czvf docs.tar.gz docs
 scp docs.tar.gz wfccn:/var/wildfirechat_sites/
 ssh wfccn 'cd /var/wildfirechat_sites; rm -rf docs; tar -xzvf docs.tar.gz; rm -rf docs.tar.gz'
